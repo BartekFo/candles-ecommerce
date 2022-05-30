727be1b1032daa3f9cfe5a20f3bd23ef0209bef4
Bartosz Fórmanowski <bartosz.f@masterborn.com>
2022-05-30 15:06:12 +0200

    feat: added example component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+const Home: NextPage = () => <button className="btn">Button</button>;
 
 export default Home;

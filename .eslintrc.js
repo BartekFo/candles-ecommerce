53a1c2e01760a3ea0602edceca0dea3a44e90125
Bartosz Fórmanowski <72879738+BartekFo@users.noreply.github.com>
2022-05-31 12:20:39 +0200

    feat: Initialize frontend (#1)

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['@masterborn/eslint-config/frontend/typescript', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    project: 'tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'prettier/prettier': ['error'],
+    'prefer-arrow-callback': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => <h2 className="btn">Button</h2>;
+
+export default Home;
